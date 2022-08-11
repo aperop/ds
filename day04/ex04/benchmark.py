403038da4c0e8b68ffddf413b44252a0e4f16bec
Ildar <dhawkgir@student.21-school.ru>
2022-08-11 23:11:47 +0300

    d04ex04

--- day04/ex04/benchmark.py
+++ day04/ex04/benchmark.py
@@ -1,3 +1,4 @@
+import numbers
 import timeit
 import random
 from collections import Counter
@@ -7,10 +8,22 @@ def generate():
     return [random.randrange(0, 101) for _ in range(1000000)]
 
 
-def counter(list):
-    count = {i: }
+def counter(lst):
+    return {i: lst.count(i) for i in set(lst)}
 
+def counter_top(lst):
+    count_dict = {i: lst.count(i) for i in set(lst)}
+    my_list = list(count_dict.items())    
+    return dict(sorted(my_list, key=lambda x: -x[1])[:10])
+
+
+def benchmark(lst):
+    print("my function:", timeit.timeit(lambda: counter(lst), number=1))
+    print("Counter:", timeit.timeit(lambda: Counter(lst), number=1))
+    print("my top:", timeit.timeit(lambda: counter_top(lst), number=1))
+    print("Counter's top:", timeit.timeit(lambda: Counter(lst).most_common(10), number=1))
 
 
 if __name__ == "__main__":
-    rand_list = generate()
+    benchmark(generate())
+
