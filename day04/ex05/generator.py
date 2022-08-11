34b80d62aaeb252c3cb62be851b7ca2b04785b16
Ildar <dhawkgir@student.21-school.ru>
2022-08-12 00:04:24 +0300

    finishd04

---
+++ day04/ex05/generator.py
@@ -0,0 +1,19 @@
+# /opt/goinfre/dhawkgir/ml-25m/ratings.csv
+
+import sys
+import resource as res
+
+def generator(path):
+    with open(path, 'r') as file:
+        for line in file:
+            yield line
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        gen = generator(sys.argv[1])
+        for i in gen:
+            pass
+        usage = res.getrusage(res.RUSAGE_SELF) 
+        print(f"Peak Memory Usage = {usage.ru_maxrss / (1024**3):0.3f} GB")
+        print(f"User Mode Time + System Mode Time = {usage.ru_utime + usage.ru_stime:0.2f}s")
